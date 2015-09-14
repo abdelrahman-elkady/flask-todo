b601bfdee8bb7c12fd595c2035ba39f43d9d50b1
kady <elkadyabdelrahman@gmail.com>
2015-09-14 15:02:00 +0200

    Adding routing list to commands

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from flask.ext.script import Manager
 from app import create_app
 from app.models import db
+from flask import url_for
 
 app = create_app()
 manager = Manager(app)
@@ -19,6 +20,24 @@ def drop_db():
 @manager.command
 def init_db():
     db.create_all()
+
+@manager.command
+def list_routes():
+    import urllib
+    output = []
+    for rule in app.url_map.iter_rules():
+
+        options = {}
+        for arg in rule.arguments:
+            options[arg] = "[{0}]".format(arg)
+
+        methods = ','.join(rule.methods)
+        url = url_for(rule.endpoint, **options)
+        line = urllib.unquote("{:40s} {:30s} {}".format(rule.endpoint, methods, url))
+        output.append(line)
+
+    for line in sorted(output):
+        print line
 
 
 if __name__ == '__main__':
