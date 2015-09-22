53bbdd80d6561143d9e259d86c0105d7480ddda6
kady <elkadyabdelrahman@gmail.com>
2015-09-22 15:15:35 +0200

    Adding more tests

--- tests/user_tests.py
+++ tests/user_tests.py
@@ -2,15 +2,19 @@ import unittest
 
 from app import create_app
 from app.models import db
+from app.models.user import User
 
 from .config_test import TestConfig
 
 app = create_app(TestConfig)
 
+
 class TestCase(unittest.TestCase):
 
     def setUp(self):
         self.app = app.test_client()
+
+        # FIXME should we really do our db interaction with the test_context ?
         with app.test_request_context():
             db.create_all()
 
@@ -21,7 +25,29 @@ class TestCase(unittest.TestCase):
 
 
 class UserTest(TestCase):
-    
-    def test_index_exists(self):
-        result = self.app.get('/')
-        assert 'INDEX' in result.data
+
+    def login(self, email, password):
+        """ helper for loggin in a user """
+        return self.app.post('/login',
+                             data={'email': email, 'password': password},
+                             follow_redirects=True
+                             )
+
+    def logout(self):
+        """helper for loggin out a user """
+        return self.app.get('/logout', follow_redirects=True)
+
+    def test_login_reachable(self):
+        """ testing login page is reachable """
+        result = self.app.get('/login')
+        assert 'Sign in' in result.data
+
+    def test_login_valid(self):
+        """ testing logging in with a valid user """
+        user = User('test_user', 'test_password', 'testmail@testprovider.com')
+        with app.test_request_context():
+            db.session.add(user)
+            db.session.commit()
+
+        res = self.login('testmail@testprovider.com', 'test_password')
+        assert 'Logged in successfully' in res.data
