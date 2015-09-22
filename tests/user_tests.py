b027fb0a119cacee2fd9d1a9e8aeb606852b925b
kady <elkadyabdelrahman@gmail.com>
2015-09-22 16:44:30 +0200

    Using Flask-Testing

--- tests/user_tests.py
+++ tests/user_tests.py
@@ -1,4 +1,4 @@
-import unittest
+from flask.ext.testing import TestCase
 
 from app import create_app
 from app.models import db
@@ -6,48 +6,48 @@ from app.models.user import User
 
 from .config_test import TestConfig
 
-app = create_app(TestConfig)
+class BaseTestCase(TestCase):
 
+    SQLALCHEMY_DATABASE_URI = "postgresql:///todo_test"
+    TESTING = True
 
-class TestCase(unittest.TestCase):
+    def create_app(self):
+        app = create_app(TestConfig)
+        return app
 
     def setUp(self):
-        self.app = app.test_client()
-
-        # FIXME should we really do our db interaction with the test_context ?
-        with app.test_request_context():
-            db.create_all()
+        db.create_all()
 
     def tearDown(self):
-        with app.test_request_context():
-            db.session.remove()
-            db.drop_all()
+        db.session.remove()
+        db.drop_all()
 
 
-class UserTest(TestCase):
+class UserTest(BaseTestCase):
 
     def login(self, email, password):
         """ helper for loggin in a user """
-        return self.app.post('/login',
+        return self.client.post('/login',
                              data={'email': email, 'password': password},
                              follow_redirects=True
                              )
 
     def logout(self):
         """helper for loggin out a user """
-        return self.app.get('/logout', follow_redirects=True)
+        return self.client.get('/logout', follow_redirects=True)
 
     def test_login_reachable(self):
         """ testing login page is reachable """
-        result = self.app.get('/login')
+        result = self.client.get('/login')
         assert 'Sign in' in result.data
 
     def test_login_valid(self):
         """ testing logging in with a valid user """
         user = User('test_user', 'test_password', 'testmail@testprovider.com')
-        with app.test_request_context():
-            db.session.add(user)
-            db.session.commit()
+
+        db.session.add(user)
+        db.session.commit()
 
         res = self.login('testmail@testprovider.com', 'test_password')
         assert 'Logged in successfully' in res.data
+        assert 'Hi test_user!' in res.data
