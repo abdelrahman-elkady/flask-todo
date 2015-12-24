befb405bdd52cb416c7d024325be6b6043f90b57
kady <elkadyabdelrahman@gmail.com>
2015-12-24 17:59:20 +0200

    Auto-closing alerts after 3 sec

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-  $(document).foundation('alert','events');
+  $(document).foundation();
+
+  var alertTimeoutId = setTimeout(function(){
+    $('.alert-box a.close').trigger('click');
+  }, 3000);
+
+  $('.alert-box a.close').click(function(){
+    clearTimeout(alertTimeoutId);
+  });
+
+
 });
